4eb092e340565845c0ebb74403cc1da2d7cd260a
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-22 13:53:28 -0500

    Whoops

--- api/v1alpha1/models/generate.go
+++ api/v1alpha1/models/generate.go
@@ -1,3 +1,3 @@
 package models
 
-//go:generate go tool oapi-codegen -config firewall_rule.oapi.yml ../../../schemas/openapi.yml
+//go:generate go tool oapi-codegen -config firewallrule.oapi.yml ../../../schemas/openapi.yml
